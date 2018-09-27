ea48985ae3fc1180778190784aba6f51004a6b6d
shnnny@163.com <3231qqww>
2018-09-28 01:43:28 +0800

    【fixbug - get comments 404】

--- src/main/java/com/blog/my/website/service/impl/SiteServiceImpl.java
+++ src/main/java/com/blog/my/website/service/impl/SiteServiceImpl.java
@@ -183,7 +183,7 @@ public class SiteServiceImpl implements ISiteService {
         if (null != archives) {
             archives.forEach(archive -> {
                 ContentVoExample example = new ContentVoExample();
-                ContentVoExample.Criteria criteria = example.createCriteria().andTypeEqualTo(Types.ARTICLE.getType()).andStatusEqualTo(Types.PUBLISH.getType());
+                ContentVoExample.Criteria criteria = example.createCriteria().andTypeEqualTo(Types.ARTICLE.getType()).andStatusEqualTo(Types.PUBLISH.getType()).andIsDeleteEqualTo(0);
                 example.setOrderByClause("created desc");
                 String date = archive.getDate();
                 Date sd = DateKit.dateFormat(date, "yyyy年MM月");
